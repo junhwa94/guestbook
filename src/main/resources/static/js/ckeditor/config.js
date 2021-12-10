a93dd55b4c2eaba366d598c4b82da799a0a2a314
junhwa94 <junhwa94>
2021-12-10 14:03:10 +0900

    ck에디터 이미지 불러오기 구현

--- src/main/resources/static/js/ckeditor/config.js
+++ src/main/resources/static/js/ckeditor/config.js
@@ -35,4 +35,10 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	config.filebrowserBrowseUrl = "../ckfinder/ckfinder.html";
+        config.filebrowserFlashBrowseUrl = "../ckfinder/ckfinder.html?type=Flash";
+        config.filebrowserUploadUrl = "../ckfinder/core/connector/java/connctor.java?command=QuickUpload&type=Files";
+        config.filebrowserImageUploadUrl = "../ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Images";
+        config.filebrowserFlashUploadUrl = "../ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Flash";
+
 };
